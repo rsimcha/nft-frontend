ecb48a153a88c8646d5d90cd05a0a1c1b7a6d04a
Thomas H <t.hanna3332@gmail.com>
2021-07-19 19:03:12 -0400

    cleaned up nav-bar a bit, created piece component for individual art pieces, my head hurts idk

---
+++ src/components/Piece.js
@@ -0,0 +1,15 @@
+import React from 'react'; 
+import { Navbar, Row, Col, Nav, Container, Button, Form, NavDropdown, FormControl } from 'react-bootstrap';
+
+const Piece = (props) => (
+      
+    <div>
+   
+        
+           
+           
+
+  </div>
+);
+
+export default Piece;
